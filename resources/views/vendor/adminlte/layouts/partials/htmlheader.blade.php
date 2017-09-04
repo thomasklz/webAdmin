4c4be36a931882c664149df806a5fa434d135e27
thomas <piposrgt@gmail.com>
2017-09-04 22:58:39 +0200

    diseño publicar noticia

--- resources/views/vendor/adminlte/layouts/partials/htmlheader.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/htmlheader.blade.php
@@ -8,7 +8,8 @@
     <link href="{{ asset('/css/adminlte/dataTables.bootstrap.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('/css/all.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('/css/adminlte/mycss.css') }}" rel="stylesheet" type="text/css" />
-
+    
+    @yield('css-top')
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
--- resources/views/vendor/adminlte/noticia/registrar.blade.php
+++ resources/views/vendor/adminlte/noticia/registrar.blade.php
@@ -3,141 +3,150 @@
 @section('htmlheader_title')
     {{ trans('adminlte_lang::message.home') }}
 @endsection
+@section('css-top')
+
+<link rel="stylesheet" href=" {{ asset('css/wizar/form-elements.css') }}" rel="stylesheet" type="text/css" >
+<link rel="stylesheet" href=" {{ asset('css/wizar/font-awesome.min.css') }}" rel="stylesheet" type="text/css" >
+<link rel="stylesheet" href=" {{ asset('css/wizar/style.css') }}" rel="stylesheet" type="text/css" >
+@endsection
 @section('main-content')
     <div class="container-fluid spark-screen">
-        <div class="box box-warning">
-            <div class="box-header with-border">
-              <h3 class="box-title">Registrar noticia</h3>
+      <div class="row">
+        <div class="col-md-12">
+          <form role="form" action="" method="post" class="f1">
+            <h3>Registrar noticias</h3>
+            <p>Ingresar toda la información necesaria para publicar la noticia</p>
+            <div class="f1-steps">
+              <div class="f1-progress">
+                <div class="f1-progress-line" data-now-value="16.66" data-number-of-steps="3" style="width: 16.66%;"></div>
+              </div>
+              <div class="f1-step active">
+                <div class="f1-step-icon" style="padding-left: 15px;"><i class="fa fa-user"></i></div>
+                <p>Noticia</p>
+              </div>
+              <div class="f1-step">
+                <div class="f1-step-icon" style="padding-left: 13px;"><i class="fa fa-key"></i></div>
+                <p>account</p>
+              </div>
+              <div class="f1-step">
+                <div class="f1-step-icon" style="padding-left: 13px;"><i class="fa fa-twitter"></i></div>
+                <p>social</p>
+              </div>
             </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-              <form role="form" class="form-horizontal">
-                 <!-- select -->
-                <div class="form-group">
-                  <label class="col-sm-2 control-label">Categoría</label>
+            <fieldset>
+              <h4>Datos de la noticia</h4>
+              <br>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">Categoría</label>
+                <div class="col-sm-10">
+                  <select class="form-control">
+                    <option>option 1</option>
+                    <option>option 2</option>
+                    <option>option 3</option>
+                    <option>option 4</option>
+                    <option>option 5</option>
+                  </select>
+                </div>
+              </div>
+              <!-- text input -->
+              <div class="form-group" >
+                <label class="col-sm-2 control-label" style="padding-top:7px;">Título</label>
+                <div class="col-sm-10" style="padding-top:7px;">
+                  <input type="text" class="form-control">
+                </div>
+              </div>
+              <!-- textarea -->
+              <div class="form-group">
+                <label class="col-sm-2 control-label" style="padding-top:7px;">Resumen</label>
+                <div class="col-sm-10" style="padding-top:7px;">
+                  <textarea class="form-control" rows="3"></textarea>
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label" style="padding-top:7px;">Contenido</label>
+                <div class="col-sm-10" style="padding-top:7px;">
+                  <textarea class="form-control" rows="3"></textarea>
+                </div>
+              </div>
+              <!-- radio -->
+              <div class="form-group">
+                <div class="row" style="padding-top:7px;">
+                  <label class="col-sm-2 control-label" style="padding-top: 7px;">Publicar:</label>
                   <div class="col-sm-10">
-                    <select class="form-control">
-                      <option>option 1</option>
-                      <option>option 2</option>
-                      <option>option 3</option>
-                      <option>option 4</option>
-                      <option>option 5</option>
-                    </select>
+                    <div class="col-md-1">
+                      <div class="radio">
+                        <label>
+                          <input type="radio" name="radio1" value="option1" checked="">
+                          Si
+                        </label>
+                      </div>
+                    </div>
+                    <div class="col-md-1">
+                      <div class="radio">
+                        <label>
+                          <input type="radio" name="radio1" value="option1">
+                          No
+                        </label>
+                      </div>
+                    </div>
                   </div>
                 </div>
-                <!-- text input -->
-                <div class="form-group">
-                  <label class="col-sm-2 control-label">Título</label>
-                   <div class="col-sm-10">
-                     <input type="text" class="form-control" placeholder="Ingresar el títuo">
-                  </div>
-                </div>
-                <!-- textarea -->
-                <div class="form-group">
-                  <label class="col-sm-2 control-label">Resumen</label>
-                  <div class="col-sm-10">
-                    <textarea class="form-control" rows="3" placeholder="Ingresar el resumen"></textarea>
-                  </div>
-                </div>
-               <div class="form-group">
-                  <label class="col-sm-2 control-label">Contenido</label>
-                  <div class="col-sm-10">
-                  <textarea class="form-control" rows="3" placeholder="Ingresar el contenido"></textarea>
-                  </div>
-                </div>
-                <!-- radio -->
-                <div class="form-group">
-                    <div class="row">
-                        <label class="col-sm-2 control-label" style="padding-top: 7px;">Publicar:</label>
-                        <div class="col-sm-10">
-                          <div class="col-md-1">
-                              <div class="radio">
-                                  <label>
-                                    <input type="radio" name="radio1" value="option1" checked="">
-                                    Si
-                                  </label>
-                              </div>
-                          </div>
-                          <div class="col-md-1">
-                              <div class="radio">
-                                  <label>
-                                    <input type="radio" name="radio1" value="option1">
-                                    No
-                                  </label>
-                              </div>
-                          </div>
-                        </div>
+              </div>
+              <div class="f1-buttons">
+                  <button type="button" class="btn btn-next">Next</button>
+              </div>
+            </fieldset>
+
+                            <fieldset>
+                                <h4>Set up your account:</h4>
+                                <div class="form-group">
+                                    <label class="sr-only" for="f1-email">Email</label>
+                                    <input type="text" name="f1-email" placeholder="Email..." class="f1-email form-control" id="f1-email">
+                                </div>
+                                <div class="form-group">
+                                    <label class="sr-only" for="f1-password">Password</label>
+                                    <input type="password" name="f1-password" placeholder="Password..." class="f1-password form-control" id="f1-password">
+                                </div>
+                                <div class="form-group">
+                                    <label class="sr-only" for="f1-repeat-password">Repeat password</label>
+                                    <input type="password" name="f1-repeat-password" placeholder="Repeat password..." 
+                                                        class="f1-repeat-password form-control" id="f1-repeat-password">
+                                </div>
+                                <div class="f1-buttons">
+                                    <button type="button" class="btn btn-previous">Previous</button>
+                                    <button type="button" class="btn btn-next">Next</button>
+                                </div>
+                            </fieldset>
+
+                            <fieldset>
+                                <h4>Social media profiles:</h4>
+                                <div class="form-group">
+                                    <label class="sr-only" for="f1-facebook">Facebook</label>
+                                    <input type="text" name="f1-facebook" placeholder="Facebook..." class="f1-facebook form-control" id="f1-facebook">
+                                </div>
+                                <div class="form-group">
+                                    <label class="sr-only" for="f1-twitter">Twitter</label>
+                                    <input type="text" name="f1-twitter" placeholder="Twitter..." class="f1-twitter form-control" id="f1-twitter">
+                                </div>
+                                <div class="form-group">
+                                    <label class="sr-only" for="f1-google-plus">Google plus</label>
+                                    <input type="text" name="f1-google-plus" placeholder="Google plus..." class="f1-google-plus form-control" id="f1-google-plus">
+                                </div>
+                                <div class="f1-buttons">
+                                    <button type="button" class="btn btn-previous">Previous</button>
+                                    <button type="submit" class="btn btn-submit">Submit</button>
+                                </div>
+                            </fieldset>
+                      
+                      </form>
                     </div>
                 </div>
-                <div class="col-sm-2">
-                </div>
-                <div class="col-sm-10">
-                <button type="button" class="btn btn-block btn-success">Registrar</button>
-                </div>
-              </form>
-            </div>
-        </div>
-        <div class="box">
-            <div class="box-header">
-              <h3 class="box-title">Lista de noticias</h3>
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-              <table id="example1" class="table table-bordered table-striped">
-                <thead>
-                <tr>
-                  <th>#Noticia</th>
-                  <th>Título</th>
-                  <th>Publicar</th>
-                  <th>Fecha Publicación</th>
-                  <th class="text-center">Acciones</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                  <td>Trident</td>
-                  <td>Internet
-                    Explorer 4.0
-                  </td>
-                  <td>Win 95+</td>
-                  <td> 4</td>
-                  <td class="text-center">
-                    <a href="#"><span class="text-green icon"><i class='fa fa-edit'></i> </span></a>
-                    <a href="#"><span class="text-green icon"><i class='fa fa-trash-o'></i></span></a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Trident</td>
-                  <td>Internet
-                    Explorer 5.0
-                  </td>
-                  <td>Win 95+</td>
-                  <td>5</td>
-                  <td class="text-center">
-                    <a href="#"><span class="text-green icon"><i class='fa fa-edit'></i> </span></a>
-                    <a href="#"><span class="text-green icon"><i class='fa fa-trash-o'></i></span></a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Trident</td>
-                  <td>Internet
-                    Explorer 5.5
-                  </td>
-                  <td>Win 95+</td>
-                  <td>5.5</td>
-                  <td class="text-center">
-                    <a href="#"><span class="text-green icon"><i class='fa fa-edit'></i> </span></a>
-                    <a href="#"><span class="text-green icon"><i class='fa fa-trash-o'></i></span></a>
-                  </td>
-                </tr>
-                </tbody>
-              </table>
-            </div>
-            <!-- /.box-body -->
-          </div>
-          <!-- /.box -->
     </div>
 @endsection
-
+@section('scripts-button')
+<script src=" {{ asset('js/wizar/jquery.backstretch.min.js') }}"></script>
+<script src=" {{ asset('js/wizar/retina-1.1.0.min.js') }}"></script>
+<script src=" {{ asset('js/wizar/scripts.js') }}"></script>
+@endsection
 <!-- Left side column. contains the logo and sidebar -->
 
